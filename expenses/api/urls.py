70fb816920d51ce1414852a7aef423aae52f1bef
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2024-04-15 23:51:02 +0800

    Set up views for creating new account

--- expenses/api/urls.py
+++ expenses/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("hello-world/", views.hello_world, name="hello_world"),
+    path("sign-up/", views.sign_up, name="sign_up"),
 ]
--- expenses/api/views.py
+++ expenses/api/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from django.core.management.utils import get_random_secret_key
-from dotenv import load_dotenv
-import os
+from django.contrib.auth.models import User
+from django.db.models import Q
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def hello_world(request):
-    load_dotenv()
-    print(get_random_secret_key())
-    print(os.getenv("SECRET_KEY"))
     return Response({"message": "Hello, world!"})
+
+
+@api_view(["POST"])
+def sign_up(request):
+    username, email, password = (
+        request.data["username"],
+        request.data["email"],
+        request.data["password"],
+    )
+
+    print(username, email, password)
+
+    try:
+        user = User.objects.filter(Q(username=username) | Q(email=email))
+        if user.exists():
+            raise ValueError("Username/Email already exists!")
+
+    except ValueError as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+    user = User.objects.create_user(
+        username=request.data["username"],
+        email=request.data["email"],
+        password=request.data["password"],
+    )
+    # TODO return auth token, username and email
+    return Response({"message": "Hello, user!"})
