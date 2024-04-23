db4cb567f57c10a7cc1cd944805bbb398640fc22
Edmund Teo Chang Hao <ainurlindale@gmail.com>
2024-04-23 11:06:17 +0800

    Added models for expenses and category

--- expenses/api/models.py
+++ expenses/api/models.py
@@ -9,3 +9,16 @@ class Income(models.Model):
     amount = models.FloatField()
     is_monthly_recurrring = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Expense(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    category = models.TextField()
+    notes = models.TextField()
+    is_monthly_recurrring = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    type = models.TextField()
